4a2d3a21957c32e06a452a5f1b82061b66ab3490
Roushan <syedroushan8539@gmail.com>
2021-10-04 19:49:31 +0530

    Angular Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,85 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dice-Game';
+  public name = "Roushan";
+  tableStyle = "solid 2px black";
+  prop = undefined;
+  isSpecial = true;
+
+  print(){
+    console.log("Hey Bitch!");
+    this.name = "omar";
+  }
+
+  danger = "danger";
+  success = "success";
+  special = "special";
+
+  classes = "success special";
+
+  hasError = true;
+
+  stylesObject = {
+    "success": !this.hasError,
+    "danger": this.hasError,
+    "special": this.isSpecial
+  }
+
+  classPrecedanceObject = {
+    "success": false,
+    "special": this.isSpecial
+  }
+
+  styleString = "color: aqua; font-size: 3rem";
+
+  styleObject = {
+    "color": "blue",
+    "font-size": "2rem",
+    "border" : "2px solid red"
+  }
+
+  nameColor = "green";
+  fontSize = 5;
+
+  onClick(){
+    console.log("Button was clicked");
+  }
+
+  greeting = '';
+
+  onClickEvent(event: any){
+    console.log(event);
+    this.greeting = this.greeting ? '' : "Hello World";
+  }
+
+  syncData = '';
+
+  setUpperCaseData(event: any){
+
+    console.log(event);
+    event = event.toUpperCase();
+    console.log(typeof(event));
+    this.syncData = event.toUpperCase();
+  }
+
+  firstExample = null;
+
+  // Structural Directives
+
+  displayGreeting = true;
+
+  items = [1,2,3,4,5,5,6,7,8,8,9];
+
+  changeArray(){
+    this.items[this.items.length - 1]++;
+    console.log(this.items);
+    
+  }
+
+  trackByItems(index: number, item: any): number { 
+    return item; 
+  }
+
+  color = "gray";
+
 }
